21dca84c121441d910cd7faedbd6a37d1c58efd4
rodrigo <24rodrigocurso@gmail.com>
2024-08-19 13:57:19 -0300

    sistema bancario simples

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,39 @@
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+        System.out.println("Por favor digite o numero da agencia! depois pressione Enter");
+        int numeroUsuario = scanner.nextInt();
+
+        System.out.println("Qual é sua agencia ?");
+        String agencia = scanner.next();
+
+        
+        System.out.println("Digite o nome do cliente");
+        String nome = scanner.next();
+
+        System.out.println("Digite o sobrenome do cliente");
+        String sobrenome = scanner.next();
+
+        System.out.println("Informe o saldo  a ser depositado!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+nome+" "+sobrenome+"  obrigado por criar uma conta em nosso banco, sua agência é "+
+         agencia+ ", conta "+numeroUsuario+ " e seu saldo "+saldo+ " já está disponível para saque");
+
+
+
+
+
+     
 
         //TODO: Conhecer e importar a classe Scanner
 
