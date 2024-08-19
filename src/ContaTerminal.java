0029ef098a7872fe1aa499231c2dea9ceed26ba5
rodrigo <24rodrigocurso@gmail.com>
2024-08-19 13:31:45 -0300

    criando aplicação de conta bancaria

---
+++ src/ContaTerminal.java
@@ -0,0 +1,17 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        //TODO: Conhecer e importar a classe Scanner
+
+
+        //Exibir as mensagens para nosso usuario
+
+
+        //Obter  pela classe scanner os valores digitados no terminal
+
+
+
+        //exibir a mensagem final
+    }
+}
